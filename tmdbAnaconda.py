cac7bb1ce1434231b25f957138fff1b28b5b4566
Niraj Shah <niraj.ns.shah@gmail.com>
2018-01-12 17:43:27 -0500

    Get all records from all Pages using the API
    Converted byte obj to dict obj -> pages[list[dicts]] -> dict -> list

--- tmdbAnaconda.py
+++ tmdbAnaconda.py
@@ -1,8 +1,29 @@
 #https://urllib3.readthedocs.io/en/latest/
 
 import urllib3 #anaconda lib NOT otherwise maybe
+import json
 
 http = urllib3.PoolManager()
 
-r = http.request('GET',"https://api.themoviedb.org/4/list/2/?api_key=a49c725845c9487dad1ee0e3748d6613")
-print(r.data)
+lst = []
+count = 0
+for i in range(1,11):
+    link = "https://api.themoviedb.org/4/discover/movie?primary_release_date.gte=1990-09-15&primary_release_date.lte=2017-10-22&page=" + str(i) + "&api_key=a49c725845c9487dad1ee0e3748d6613"
+    r = http.request('GET',link)
+    
+    #Converted byte obj to dict obj. 
+    dict = json.loads(r.data)
+    #Got the list object from the dict
+    movieList = dict['results']
+    
+    # looped through each dict in the List
+    for movie in movieList:
+        count=count+1
+        #created a list from all the dicts appended
+        lst.append(movie)
+    # print()
+    # lst.append(dict['results'])
+
+print(lst)
+
+#content = urllib3.urlopen("https://api.themoviedb.org/4/list/2/?api_key=a49c725845c9487dad1ee0e3748d6613").read()
